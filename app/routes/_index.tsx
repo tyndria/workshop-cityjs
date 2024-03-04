c21c0d17c35a708e542d400f2e6d444c1ed43e4f
Alexandra Spalato <alexaspalato@gmail.com>
2024-03-04 19:16:34 +0100

    shadcn

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
+import { Button } from "~/components/ui/button";
 
 export const meta: MetaFunction = () => {
   return [
@@ -11,6 +12,7 @@ export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
       <h1 className="uppercase font-bold text-green-400">Welcome to Remix</h1>
+      <Button>Click me</Button>
       <ul>
         <li>
           <a
