7ff4292bb01fc18a205707cb8f9655c27226510c
Alexandra Spalato <alexaspalato@gmail.com>
2024-03-06 19:10:44 +0100

    Refactor EditPost component

--- app/components/EditPost.tsx
+++ app/components/EditPost.tsx
@@ -5,15 +5,18 @@ import { Button } from "~/components/ui/button";
 
 const EditPost = ({ post }) => {
   return (
-    <Form method="post" action="dashboard">
-      <Input type="text" value={post.title} />
-      <Editor
-        apiKey="megl6butiqhm3whiwmspl4igyb05ob2u5zke3i53jduwwma6"
-        value={post.content}
-      />
-      <input type="hidden" name="task" value="edit" />
-      <Button>Submit</Button>
-    </Form>
+    <div>
+      <h1>Edit your post</h1>
+      <Form method="post" action="dashboard" className="space-y-5">
+        <Input type="text" value={post.headline} />
+        <Editor
+          apiKey="megl6butiqhm3whiwmspl4igyb05ob2u5zke3i53jduwwma6"
+          value={post.content}
+        />
+        <input type="hidden" name="task" value="edit" />
+        <Button>Submit</Button>
+      </Form>
+    </div>
   );
 };
 
