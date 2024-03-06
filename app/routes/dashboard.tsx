c01748b402d831e1a1d4d795814272cb2bcd06b2
Alexandra Spalato <alexaspalato@gmail.com>
2024-03-06 15:33:03 +0100

    Add button to create new posts

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -1,6 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 import { LoaderFunction, json } from "@remix-run/node";
 import { Link, useLoaderData } from "@remix-run/react";
+import { Button } from "~/components/ui/button";
 
 const prisma = new PrismaClient();
 
@@ -14,18 +15,23 @@ const Blog = () => {
   const { posts } = useLoaderData();
 
   return (
-    <div className="max-w-sm mx-auto">
-      <h1>Blog</h1>
-      {posts.map((post) => (
-        <h2 key={post.id}>
-          <Link
-            to={`/${post.slug}`}
-            className="bg-gradient-to-r font-bold from-purple-500 to-pink-500 text-transparent bg-clip-text transition duration-500 hover:from-purple-400 hover:to-pink-400"
-          >
-            {post.title}
-          </Link>
-        </h2>
-      ))}
+    <div className="flex justify-between gap-5">
+      <div className="border border-border p-10 w-full">
+        <h1>Posts</h1>
+        {posts.map((post) => (
+          <h2 key={post.id}>
+            <Link
+              to={`/${post.slug}`}
+              className="bg-gradient-to-r font-bold from-purple-500 to-pink-500 text-transparent bg-clip-text transition duration-500 hover:from-purple-400 hover:to-pink-400"
+            >
+              {post.title}
+            </Link>
+          </h2>
+        ))}
+      </div>
+      <div className="border border-border p-10 w-full">
+        <Button className="uppercase">Create Post</Button>
+      </div>
     </div>
   );
 };
